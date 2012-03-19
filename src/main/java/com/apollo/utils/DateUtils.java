28136800730f14bf7c7c71120b59ae0930964145
DavyLin <linjxljx@gmail.com>
2012-03-19 15:03:58 +0800

    create apollo project

---
+++ src/main/java/com/apollo/utils/DateUtils.java
@@ -0,0 +1,29 @@
+/**
+ * @(#)CurrentFile
+ * 版权声明 亚信联创集团股份有限公司, 版权所有 违者必究
+ *
+ *<br> Copyright:  Copyright (c) 2012
+ *<br> Company:亚信联创集团股份有限公司 
+ *<br> @author 林建雄
+ *<br> 12-3-19 下午3:02
+ *<br> @version 1.0
+ *——————————————————————————————————————
+ *修改记录
+ *    修改者：
+ *    修改时间：
+ *    修改原因：
+ *——————————————————————————————————————
+ */
+package com.apollo.utils;
+
+/**
+ * <pre>
+ * </pre>
+ *
+ * @author linjx
+ *         JDK版本:1.6
+ * @version 1.0
+ * @since 1.0
+ */
+public class DateUtils {
+}
