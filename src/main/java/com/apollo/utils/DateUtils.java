050f6ae9652bc06343843398be4a20fd9852de0c
DavyLin <linjxljx@gmail.com>
2013-08-20 16:06:25 +0800

    update the license and readme

--- src/main/java/com/apollo/utils/DateUtils.java
+++ src/main/java/com/apollo/utils/DateUtils.java
@@ -1,19 +1,21 @@
-/**
- * @(#)CurrentFile
- * 版权声明 亚信联创集团股份有限公司, 版权所有 违者必究
+/*
+ * (C) 2013-2015 Asiainfo-linkage Group Holding Limited.
  *
- *<br> Copyright:  Copyright (c) 2012
- *<br> Company:亚信联创集团股份有限公司 
- *<br> @author 林建雄
- *<br> 12-3-19 下午3:02
- *<br> @version 1.0
- *——————————————————————————————————————
- *修改记录
- *    修改者：
- *    修改时间：
- *    修改原因：
- *——————————————————————————————————————
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * Authors:
+ *   davylin<linjxljx@gmail.com>
  */
+
 package com.apollo.utils;
 
 /**
